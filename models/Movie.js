b602fca2a7704a3293f851c6cfc6c10bf41e8e57
jonipievilainen <r1jo1jo1@gmail.com>
2023-08-29 22:51:36 +0300

    Updated sql settings for AWS connection

--- models/Movie.js
+++ models/Movie.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Varmista yhteyden toimivuus AWS RDS:ään
+    }
+  }
 });
 
 const Movie = sequelize.define('Movie', {
