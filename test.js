0aac875785fe01978e8e912b0a0f93b7d7afcc81
jonipievilainen <r1jo1jo1@gmail.com>
2023-08-30 23:41:41 +0300

    Updated unit test

--- test.js
+++ test.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
-const expect = chai.expect;
 
-describe('Movies API', () => {
-  console.log('Testit käynnissä');
-  it('should return all movies', (done) => {
-    chai.request(process.env.API_URL)
-      .get('/movies')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        done();
-      });
-  });
-  
+const expect = chai.expect;
+const Movie = require('./models/Movie');
+
+describe('Database Connection', () => {
+  it('should get all movies', async () => {
+    try {
+      const movies = await Movie.findAll();
+      expect(movies).to.be.an('array');
+    } catch (error) {
+      console.error('Database connection error:', error.message);
+      throw error;
+    }
+  }).timeout(5000);
 });
