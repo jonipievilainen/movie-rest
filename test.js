31bcdf7ef3d9aeaef0d62944d3ef6c8d64962044
Joni Pieviläinen <118271701+geniem-jonipievilainen@users.noreply.github.com>
2023-09-22 16:10:26 +0300

    Some lighthouse test

--- test.js
+++ test.js
@@ -8,23 +8,35 @@ import * as chromeLauncher from 'chrome-launcher';
 // So we can save output
 import {writeFile} from 'fs/promises';
 
-(async () => {
-    // Launch instance of Chrome
-    const chrome = await chromeLauncher.launch();
 
-    // Gather results and report from Lighthouse
-    const results = await lighthouse('https://example.com', {
-        port: chrome.port,
-        output: 'json'
-    }, {
-        extends: 'lighthouse:default',
-        settings: {
-            onlyCategories: ['performance']
+describe('lighthouse test', () => {
+    it('should get get lighthouse result', async () => {
+        try {
+            // Launch instance of Chrome
+            const chrome = await chromeLauncher.launch();
+
+            // Gather results and report from Lighthouse
+            const results = await lighthouse('https://example.com', {
+                port: chrome.port,
+                output: 'json'
+            }, {
+                extends: 'lighthouse:default',
+                settings: {
+                    onlyCategories: ['performance']
+                }
+            });
+
+            console.log('Report is done for', results.lhr.finalDisplayedUrl);
+            console.log('Performance score was', results.lhr.categories.performance.score * 100);
+
+            // print result as mocha test output
+            console.log(JSON.stringify(results.lhr, null, 2));
+
+            // Kill Chrome
+            await chrome.kill();
+        } catch (error) {
+            console.error('Error lighthouse:', error.message);
+            throw error;
         }
-    });
-
-    console.log(results.report)
-
-    // Kill Chrome
-    await chrome.kill();
-})();
+    }).timeout(20000);
+});
